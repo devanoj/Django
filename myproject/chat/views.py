09036a5d332f401b842be4ca22b84d3761aef72e
Devano <C19312821@mytudublin.ie>
2023-07-30 12:35:51 +0100

    added chat saved to realtime

--- myproject/chat/views.py
+++ myproject/chat/views.py
@@ -1,7 +1,28 @@
 # chat/views.py
-
+import firebase_admin
+from firebase_admin import credentials, db
 from django.shortcuts import render
 
 def chat_box_view(request):
-    # Your chat box view logic goes here
-    return render(request, 'chat/chat_box.html')
+    if request.method == 'POST':
+        message = request.POST.get('message', '')
+        if message.strip():
+            # Save the chat message to Firebase Realtime Database
+            messages_ref = db.reference('chat_messages')
+            new_message_ref = messages_ref.push()
+            new_message_ref.set({
+                'content': message
+            })
+
+    # Fetch chat messages from Firebase
+    messages_ref = db.reference('chat_messages')
+    messages = messages_ref.get()
+
+    # Convert Firebase response to a list of messages
+    chat_messages = [message['content'] for message in messages.values()] if messages else []
+
+    context = {
+        'chat_messages': chat_messages
+    }
+
+    return render(request, 'chat/chat_box.html', context)
