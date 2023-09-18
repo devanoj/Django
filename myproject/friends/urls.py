0cc3f8ecf82d09ff7c9733fabca5552a8a04e13b
Devano <C19312821@mytudublin.ie>
2023-09-18 22:28:57 +0100

    Added Contacts

--- myproject/friends/urls.py
+++ myproject/friends/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('friends/', views.addFriends, name='add_contacts'),  
+    path('friends/', views.addFriends, name='add_contacts'),
+    path('submit_friend/', views.submitFriends, name='submit_friend'),
 ]
--- myproject/friends/views.py
+++ myproject/friends/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from firebase_admin import db, auth
 
 def addFriends(request):
     return render(request, 'friends/friend.html')
 
-# Create your views here.
+def submitFriends(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('firstName')
+        last_name = request.POST.get('lastName')
+        email = request.POST.get('email')
+
+        # Your code for adding friends here
+        # For now, we're printing the submitted data
+        print(f"{first_name} {last_name} {email}")
+
+        user1 = auth.get_user_by_email(request.session['email'])
+        
+        return redirect('login_success', username=user1.email)
